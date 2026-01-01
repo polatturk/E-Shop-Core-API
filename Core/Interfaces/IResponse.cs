c3dfe080f7ff0d2f6b6585ce63dd918fec0510c6
polat türk <polatturr52@gmail.com>
2026-01-01 22:29:26 +0300

    feat: add IResponse and generic Response wrapper with static helpers

---
+++ Core/Interfaces/IResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IResponse
+    {
+        bool IsSuccess { get; set; }
+        int StatusCode { get; set; }
+        List<string>? Errors { get; set; }
+    }
+}
---
+++ Core/Response/Response.cs
@@ -0,0 +1,29 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Response
+{
+    public class Response<T> : IResponse
+    {
+        public T? Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public int StatusCode { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public static Response<T> Success(T data, int statusCode = 200)
+            => new() { Data = data, IsSuccess = true, StatusCode = statusCode };
+
+        public static Response<T> Success(int statusCode = 200)
+            => new() { IsSuccess = true, StatusCode = statusCode };
+
+        public static Response<T> Fail(string error, int statusCode = 400)
+            => new() { IsSuccess = false, Errors = new List<string> { error }, StatusCode = statusCode };
+
+        public static Response<T> Fail(List<string> errors, int statusCode = 400)
+            => new() { IsSuccess = false, Errors = errors, StatusCode = statusCode };
+    }
+}
