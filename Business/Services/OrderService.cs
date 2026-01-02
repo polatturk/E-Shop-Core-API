429c516b29a051c53ebd32c4ddc26b7e8d662b1c
polat türk <polatturr52@gmail.com>
2026-01-02 22:55:22 +0300

    feat: complete OrderService implementation

--- Business/Services/OrderService.cs
+++ Business/Services/OrderService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Interfaces;
+using Core.Response;
+using DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,41 @@ using System.Threading.Tasks;
 
 namespace Business.Services
 {
-    public class OrderService
+    public class OrderService(IGenericRepository<Order> _repository) : IOrderService
     {
+        public async Task<Response<List<Order>>?> GetAllAsync()
+        {
+            var data = await _repository.GetAll().ToListAsync();
+            return Response<List<Order>>.Success(data, 200);
+        }
 
+        public async Task<Response<Order>> GetByIdAsync(Guid id)
+        {
+            var data = await _repository.GetByIdAsync(id);
+            return data == null ? Response<Order>.Fail("Sipariş kaydı bulunamadı", 404) : Response<Order>.Success(data, 200);
+        }
+
+        public async Task<Response<Order>> CreateAsync(Order entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.SaveAsync();
+            return Response<Order>.Success(entity, 201);
+        }
+
+        public async Task<Response<bool>> Update(Order entity)
+        {
+            _repository.Update(entity);
+            await _repository.SaveAsync();
+            return Response<bool>.Success(204);
+        }
+
+        public async Task<Response<bool>> Remove(Guid id)
+        {
+            var data = await _repository.GetByIdAsync(id);
+            if (data == null) return Response<bool>.Fail("Sipariş kaydı bulunamadı", 404);
+            _repository.Remove(data);
+            await _repository.SaveAsync();
+            return Response<bool>.Success(204);
+        }
     }
 }
--- Core/Interfaces/IOrderService.cs
+++ Core/Interfaces/IOrderService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace Core.Interfaces
 {
     public interface IOrderService
     {
-
+        Task<Response<List<Order>>> GetAllAsync();
+        Task<Response<Order>> GetByIdAsync(Guid id);
+        Task<Response<Order>> CreateAsync(Order order);
+        Task<Response<bool>> Update(Order order);
+        Task<Response<bool>> Remove(Guid id);
     }
 }
