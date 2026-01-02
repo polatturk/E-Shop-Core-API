a7c9e0317b94d83e3b2b3befbe5fd492311a969d
polat türk <polatturr52@gmail.com>
2026-01-02 23:21:47 +0300

    feat: complete CartService implementation

--- Business/Services/CartService.cs
+++ Business/Services/CartService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Interfaces;
+using Core.Response;
+using DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,41 @@ using System.Threading.Tasks;
 
 namespace Business.Services
 {
-    public class CartService
+    public class CartService(IGenericRepository<Cart> _repository) : ICartService
     {
+        public async Task<Response<List<Cart>>> GetAllAsync()
+        {
+            var data = await _repository.GetAll().ToListAsync();
+            return Response<List<Cart>>.Success(data, 200);
+        }
 
+        public async Task<Response<Cart>> GetByIdAsync(Guid id)
+        {
+            var data = await _repository.GetByIdAsync(id);
+            return data == null ? Response<Cart>.Fail("Sepet bulunamadı", 404) : Response<Cart>.Success(data, 200);
+        }
+
+        public async Task<Response<Cart>> CreateAsync(Cart entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.SaveAsync();
+            return Response<Cart>.Success(entity, 201);
+        }
+
+        public async Task<Response<bool>> Update(Cart entity)
+        {
+            _repository.Update(entity);
+            await _repository.SaveAsync();
+            return Response<bool>.Success(204);
+        }
+
+        public async Task<Response<bool>> Remove(Guid id)
+        {
+            var data = await _repository.GetByIdAsync(id);
+            if (data == null) return Response<bool>.Fail("Sepet bulunamadı", 404);
+            _repository.Remove(data);
+            await _repository.SaveAsync();
+            return Response<bool>.Success(204);
+        }
     }
 }
--- Core/Interfaces/ICartService.cs
+++ Core/Interfaces/ICartService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace Core.Interfaces
 {
     public interface ICartService
     {
-
+        Task<Response<List<Cart>>> GetAllAsync();
+        Task<Response<Cart>> GetByIdAsync(Guid id);
+        Task<Response<Cart>> CreateAsync(Cart cart);
+        Task<Response<bool>> Update(Cart cart);
+        Task<Response<bool>> Remove(Guid id);
     }
 }
