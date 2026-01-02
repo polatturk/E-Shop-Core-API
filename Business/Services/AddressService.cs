eed53f6f06522e4e12f5d3b27514820bd1c41888
polat türk <polatturr52@gmail.com>
2026-01-02 23:24:37 +0300

    feat: complete AddressService implementation

--- Business/Services/AddressService.cs
+++ Business/Services/AddressService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Interfaces;
+using Core.Response;
+using DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,47 @@ using System.Threading.Tasks;
 
 namespace Business.Services
 {
-    public class AddressService
+    public class AddressService(IGenericRepository<Address> _repository) : IAddressService
     {
+        public async Task<Response<List<Address>>> GetAllAsync()
+        {
+            var addresses = await _repository.GetAll().ToListAsync();
+            return Response<List<Address>>.Success(addresses, 200);
+        }
 
+        public async Task<Response<Address>> GetByIdAsync(Guid id)
+        {
+            var address = await _repository.GetByIdAsync(id);
+
+            if (address == null)
+                return Response<Address>.Fail("Adres bulunamadı", 404);
+
+            return Response<Address>.Success(address, 200);
+        }
+
+        public async Task<Response<Address>> CreateAsync(Address entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.SaveAsync();
+            return Response<Address>.Success(entity, 201);
+        }
+
+        public async Task<Response<bool>> Update(Address entity)
+        {
+            _repository.Update(entity);
+            await _repository.SaveAsync();
+            return Response<bool>.Success(204);
+        }
+
+        public async Task<Response<bool>> Remove(Guid id)
+        {
+            var address = await _repository.GetByIdAsync(id);
+            if (address == null)
+                return Response<bool>.Fail("Adres bulunamadı", 404);
+
+            _repository.Remove(address);
+            await _repository.SaveAsync();
+            return Response<bool>.Success(204);
+        }
     }
 }
--- Core/Interfaces/IAddressService.cs
+++ Core/Interfaces/IAddressService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace Core.Interfaces
 {
     public interface IAddressService
     {
-
+        Task<Response<List<Address>>> GetAllAsync();
+        Task<Response<Address>> GetByIdAsync(Guid id);
+        Task<Response<Address>> CreateAsync(Address address);
+        Task<Response<bool>> Update(Address address);
+        Task<Response<bool>> Remove(Guid id);
     }
 }
