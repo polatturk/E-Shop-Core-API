7fd2b7d8f3279127bbbe57e9c3760b5673159f81
polat türk <polatturr52@gmail.com>
2026-01-03 23:19:23 +0300

    feat: add order and orderitem dtos as records

---
+++ Core/DTOs/OrderDtos.cs
@@ -0,0 +1,12 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record OrderCreateDto(Guid UserId, Guid ShippingAddressId, Guid BillingAddressId, List<OrderItemCreateDto> OrderItems);
+    public record OrderResponseDto(Guid Id, DateTime OrderDate, decimal TotalAmount, OrderStatus Status, List<OrderItemResponseDto> OrderItems);
+}
---
+++ Core/DTOs/OrderItemDtos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record OrderItemCreateDto(Guid ProductId, int Quantity, decimal UnitPrice);
+
+    public record OrderItemResponseDto(Guid Id, Guid ProductId, string ProductName, int Quantity, decimal UnitPrice, decimal TotalPrice);
+}
