c2bae85618885ba23cf0926ab5eb9765ea9ab709
polat türk <polatturr52@gmail.com>
2026-01-03 23:00:18 +0300

    feat: add payment dtos as records

---
+++ Core/DTOs/PaymentDtos.cs
@@ -0,0 +1,15 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record PaymentCreateDto(decimal Amount, PaymentMethod PaymentMethod, Guid OrderId);
+
+    public record PaymentResponseDto(Guid Id, decimal Amount, DateTime PaymentDate, PaymentMethod PaymentMethod, PaymentStatus Status, Guid OrderId);
+
+    public record PaymentStatusUpdateDto(Guid Id, PaymentStatus Status);
+}
