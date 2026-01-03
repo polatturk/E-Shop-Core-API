d03ac38ae815706b58998becd78f55d80fc3f600
polat türk <polatturr52@gmail.com>
2026-01-03 21:20:29 +0300

    feat: add product dtos as records

---
+++ Core/DTOs/ProductDtos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record ProductCreateDto(string Name, string Description, decimal Price, int Stock, int CategoryId);
+
+    public record ProductUpdateDto(int Id, string Name, string Description, decimal Price, int Stock, int CategoryId);
+
+    public record ProductResponseDto(int Id, string Name, string Description, decimal Price, int Stock, string CategoryName);
+}
