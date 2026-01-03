9917ba539c203628a343668c17f72737ace4ba26
polat türk <polatturr52@gmail.com>
2026-01-03 21:47:10 +0300

    feat: add user dtos as records

---
+++ Core/DTOs/UserDtos.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record UserRegisterDto(string FirstName, string LastName, string Email, string Password, Gender Gender);
+
+    public record UserLoginDto(string Email, string Password);
+
+    public record UserResponseDto(Guid Id, string FullName, string Email, UserRole Role, Gender Gender);
+
+    public record UserUpdateDto(Guid Id, string FirstName, string LastName, string Email, Gender Gender);
+}
