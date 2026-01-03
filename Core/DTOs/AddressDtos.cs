61339a9ea6c9cca6f1cc0e0fbbb8905fdea3981c
polat türk <polatturr52@gmail.com>
2026-01-03 23:38:25 +0300

    feat: add address dtos as records

---
+++ Core/DTOs/AddressDtos.cs
@@ -0,0 +1,15 @@
+﻿using Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record AddressCreateDto(string Street, string City, string State, string Country, string ZipCode, AddressType Type, Guid UserId);
+
+    public record AddressUpdateDto(Guid Id, string Street, string City, string State, string Country, string ZipCode, AddressType Type);
+
+    public record AddressResponseDto(Guid Id, string Street, string City, string State, string Country, string ZipCode, AddressType Type);
+}
