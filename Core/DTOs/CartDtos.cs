48573c6f13aa0fa33e495ee47ed40cbfeadab71a
polat türk <polatturr52@gmail.com>
2026-01-03 23:34:00 +0300

    feat: add cart and cartitem dtos as records

---
+++ Core/DTOs/CartDtos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record CartResponseDto(Guid Id, Guid UserId, decimal TotalAmount, List<CartItemResponseDto> Items);
+}
---
+++ Core/DTOs/CartItemDtos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record AddToCartDto(Guid ProductId, int Quantity);
+
+    public record CartItemUpdateDto(Guid Id, int Quantity);
+
+    public record CartItemResponseDto(Guid Id, Guid ProductId, string ProductName, string ImageUrl, int Quantity, decimal UnitPrice, decimal TotalPrice);
+}
