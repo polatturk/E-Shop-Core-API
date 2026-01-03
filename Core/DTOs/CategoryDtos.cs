5a61708b03c01a06755744857adb2ff68b67f391
polat türk <polatturr52@gmail.com>
2026-01-03 21:38:05 +0300

    feat: add category dtos as records

---
+++ Core/DTOs/CategoryDtos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public record CategoryCreateDto(string Name, string? Description);
+
+    public record CategoryUpdateDto(Guid Id, string Name, string? Description);
+
+    public record CategoryResponseDto(Guid Id, string Name, string? Description);
+
+}
