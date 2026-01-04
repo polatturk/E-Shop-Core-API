7fe6f13425edec55a3a95c893c86e7faf4f2ac20
polat türk <polatturr52@gmail.com>
2026-01-04 16:05:00 +0300

    feat: implement ProductMapper using Mapperly

---
+++ Core/Mappings/ProductMapper.cs
@@ -0,0 +1,24 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    public partial class ProductMapper
+    {
+        public partial Product ToEntity(ProductCreateDto dto);
+
+        public partial Product ToEntity(ProductUpdateDto dto);
+
+        [MapProperty(nameof(Product.Category.Name), nameof(ProductResponseDto.CategoryName))]
+        public partial ProductResponseDto ToResponseDto(Product product);
+
+        public partial List<ProductResponseDto> ToResponseDtoList(List<Product> products);
+    }
+}
