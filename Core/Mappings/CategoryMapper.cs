af4166f1612fb395a064ddb15c3d2ad9b118f226
polat türk <polatturr52@gmail.com>
2026-01-04 21:52:20 +0300

    feat: implement CategoryMapper using Mapperly

---
+++ Core/Mappings/CategoryMapper.cs
@@ -0,0 +1,27 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    public partial class CategoryMapper
+    {
+        // CreateDto -> Entity
+        public static partial Category ToEntity(CategoryCreateDto dto);
+
+        // UpdateDto -> Entity
+        public static partial Category ToEntity(CategoryUpdateDto dto);
+
+        // Entity -> ResponseDto
+        public static partial CategoryResponseDto ToResponseDto(Category category);
+
+        // Liste halindeki kategorileri dönüştürmek için
+        public static partial List<CategoryResponseDto> ToResponseDtoList(List<Category> categories);
+    }
+}
