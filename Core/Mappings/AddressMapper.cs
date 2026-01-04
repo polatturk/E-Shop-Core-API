0efd785001385196c6f699a6baaed822757b8973
polat türk <polatturr52@gmail.com>
2026-01-04 21:54:21 +0300

    feat: implement AddressMapper using Mapperly

---
+++ Core/Mappings/AddressMapper.cs
@@ -0,0 +1,27 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    public partial class AddressMapper
+    {
+        // CreateDto -> Entity
+        public static partial Address ToEntity(AddressCreateDto dto);
+
+        // UpdateDto -> Entity
+        public static partial Address ToEntity(AddressUpdateDto dto);
+
+        // Entity -> ResponseDto
+        public static partial AddressResponseDto ToResponseDto(Address address);
+
+        // Kullanıcın tüm adreslerini listelemek için
+        public static partial List<AddressResponseDto> ToResponseDtoList(List<Address> addresses);
+    }
+}
