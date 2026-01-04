eedcd03b5504df63dbffc215ea8298a60ad951db
polat türk <polatturr52@gmail.com>
2026-01-04 21:50:02 +0300

    feat: implement PaymentMapper using Mapperly

---
+++ Core/Mappings/PaymentMapper.cs
@@ -0,0 +1,21 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    public partial class PaymentMapper
+    {
+        public static partial Payment ToEntity(PaymentCreateDto dto);
+
+        public static partial Payment ToEntity(PaymentStatusUpdateDto dto);
+
+        public static partial PaymentResponseDto ToResponseDto(Payment payment);
+    }
+}
