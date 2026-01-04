5877b314a9ad97a1cbf120a761e7b35092c7d3b7
polat türk <polatturr52@gmail.com>
2026-01-04 16:36:58 +0300

    feat: implement OrderMapper and OrderItemMapper using Mapperly

---
+++ Core/Mappings/OrderItemMapper.cs
@@ -0,0 +1,25 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    public partial class OrderItemMapper
+    {
+        [MapProperty("Product.Name", nameof(OrderItemResponseDto.ProductName))]
+
+        [MapProperty(nameof(OrderItem.Quantity), nameof(OrderItemResponseDto.TotalPrice))]
+        public static partial OrderItemResponseDto ToResponseDto(OrderItem item);
+
+        private static decimal MapTotalPrice(OrderItem item)
+        {
+            return item.Quantity * item.UnitPrice;
+        }
+    }
+}
---
+++ Core/Mappings/OrderMapper.cs
@@ -0,0 +1,22 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    [UseStaticMapper(typeof(OrderItemMapper))]
+    public partial class OrderMapper
+    {
+        public partial Order ToEntity(OrderCreateDto dto);
+
+        public partial OrderResponseDto ToResponseDto(Order order);
+
+        public partial List<OrderResponseDto> ToResponseDtoList(List<Order> orders);
+    }
+}
