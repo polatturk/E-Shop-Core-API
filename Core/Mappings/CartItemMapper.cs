2053679fa8aa277584d6191d8e96398cd8668868
polat türk <polatturr52@gmail.com>
2026-01-04 17:19:09 +0300

    feat: implement CartMapper and CartItemMapper using Mapperly

---
+++ Core/Mappings/CartItemMapper.cs
@@ -0,0 +1,30 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    public partial class CartItemMapper
+    {
+        [MapProperty("Product.Name", nameof(CartItemResponseDto.ProductName))]
+
+        [MapProperty("Product.ImageUrl", nameof(CartItemResponseDto.ImageUrl))]
+
+        [MapProperty("Product.Price", nameof(CartItemResponseDto.UnitPrice))]
+
+        [MapProperty(nameof(CartItem.Quantity), nameof(CartItemResponseDto.TotalPrice))]
+
+        public static partial CartItemResponseDto ToResponseDto(CartItem item);
+
+        private static decimal MapTotalPrice(CartItem item)
+        {
+            return item.Quantity * item.Product.Price;
+        }
+    }
+}
---
+++ Core/Mappings/CartMapper.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using Riok.Mapperly.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mappings
+{
+    [Mapper(RequiredMappingStrategy = RequiredMappingStrategy.None)]
+    [UseStaticMapper(typeof(CartItemMapper))] 
+    public partial class CartMapper
+    {
+        public partial CartResponseDto ToResponseDto(Cart cart);
+    }
+}
