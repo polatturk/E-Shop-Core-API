48951b98731616e6dad09cf6660f54b05520c4fe
polat türk <polatturr52@gmail.com>
2026-01-05 21:38:08 +0300

    feat: implement Unit of Work pattern
    Added IUnitOfWork interface to the Core layer and implemented the UnitOfWork class in the DataAccess layer to manage repository transactions.

---
+++ Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> GetRepository<T>() where T : class;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Core.Interfaces;
+using DataAccess.Repository.DataAccess.Repositories;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApiContext _context;
+
+        public UnitOfWork(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public IGenericRepository<T> GetRepository<T>() where T : class
+        {
+            return new GenericRepository<T>(_context);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
