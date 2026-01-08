35a8ece0cbfbf5de93f96f48e98ec9df6a817992
polat türk <polatturr52@gmail.com>
2026-01-08 13:53:58 +0300

    feat: add ProductController for CRUD operations

---
+++ E-Shop Core API/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopCoreAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ProductController(IProductService _productService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _productService.GetAllAsync();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var response = await _productService.GetByIdAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateDto dto)
+        {
+            var response = await _productService.CreateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto dto)
+        {
+            var response = await _productService.UpdateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _productService.RemoveAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
