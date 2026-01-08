e294862a5f3f111bc9ec97332d361920f93f6414
polat türk <polatturr52@gmail.com>
2026-01-08 20:58:39 +0300

    feat: add UserController for CRUD operations

---
+++ E-Shop Core API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Business.Services;
+using Core.DTOs;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserService _userService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _userService.GetAllAsync();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var response = await _userService.GetByIdAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserRegisterDto dto)
+        {
+            var response = await _userService.CreateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UserUpdateDto dto)
+        {
+            var response = await _userService.UpdateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _userService.RemoveAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
