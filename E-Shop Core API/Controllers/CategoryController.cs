9a4600632e870e7aa1b092ed675d0873718c93c2
polat türk <polatturr52@gmail.com>
2026-01-19 09:53:57 +0300

    feat: add CategoryController for CRUD operations

---
+++ E-Shop Core API/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop_Core_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController(ICategoryService _categoryService) : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _categoryService.GetAllAsync();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var response = await _categoryService.GetByIdAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CategoryCreateDto dto)
+        {
+            var response = await _categoryService.CreateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(CategoryUpdateDto dto)
+        {
+            var response = await _categoryService.UpdateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _categoryService.RemoveAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
