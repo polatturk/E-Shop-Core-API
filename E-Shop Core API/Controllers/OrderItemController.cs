76dc065e0a20bcdbbc3140bb6099d31aa1f8b7e1
polat türk <polatturr52@gmail.com>
2026-01-24 22:34:14 +0300

    feat: add OrderItemController for CRUD operations

---
+++ E-Shop Core API/Controllers/OrderItemController.cs
@@ -0,0 +1,48 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop_Core_API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderItemController(IOrderItemService _orderItemService) : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _orderItemService.GetAllAsync();
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var response = await _orderItemService.GetByIdAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(OrderItemCreateDto dto)
+        {
+            var response = await _orderItemService.CreateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(OrderItemUpdateDto dto)
+        {
+            var response = await _orderItemService.UpdateAsync(dto);
+            return StatusCode(response.StatusCode, response);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _orderItemService.RemoveAsync(id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
